1a67ac9bb64de2d7465917db4c6dcba7bbefd8fc
Amir Greenberg <amirgggg@gmail.com>
2023-11-16 10:35:03 +0200

    Limit ball size to 400. show ball size inside ball

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 
 function onBallClick(elBall) {
     var css = getComputedStyle(elBall)
-    var cssWidth = parseInt(css.width,10)
+    var cssWidth = parseInt(css.width, 10)
     var cssHeight = css.height.match(/(\d+)/)[0]
 
-    elBall.style.width = +cssWidth +50 + 'px'
-    elBall.style.height = +cssHeight +50 + 'px'
+    if (cssWidth < 400) {
+        elBall.style.width = +cssWidth + 50 + 'px'
+        elBall.style.height = +cssHeight + 50 + 'px'
+    } else {
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+    }
+elBall.innerText = parseInt(elBall.style.width, 10)
 }
