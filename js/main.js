f3ec41664152798f8cfd4af9077ac21ebd3a1dc1
Amir Greenberg <amirgggg@gmail.com>
2023-11-16 10:20:46 +0200

    onBallClick() increment ball size by 50px

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict'
 
-function onBallClick() {
-    console.log('ball clicked!')
+function onBallClick(elBall) {
+    var css = getComputedStyle(elBall)
+    var cssWidth = parseInt(css.width,10)
+    var cssHeight = css.height.match(/(\d+)/)[0]
+
+    elBall.style.width = +cssWidth +50 + 'px'
+    elBall.style.height = +cssHeight +50 + 'px'
 }
